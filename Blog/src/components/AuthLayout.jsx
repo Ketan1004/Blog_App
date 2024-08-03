6a612acb2e86f317bb578aadffdf3802e98d23a5
Ketan1004 <ashishshrikhande250@gmail.com>
2024-08-03 18:59:46 +0530

    added

--- Blog/src/components/AuthLayout.jsx
+++ Blog/src/components/AuthLayout.jsx
@@ -8,8 +8,15 @@ export default function Protected({children, authentication=true}) {
         const [loader,setLoader] = useState(true)
         const authStatus = useSelector(state => state.auth.status)
 
+       useEffect(()=>{  
+        //TODO: make it more easy
 
-  return (
-    <div>AuthLayout</div>
-  )
+            if(authentication && authStatus !== authentication){
+             navigate('/login')
+            }else if(!authentication && authStatus !== authentication){
+             navigate("/")
+            }
+            setLoader(false)
+       } ,[authStatus, navigate, authentication])
+  return loader ? <h1>Loading...</h1> : <>{children}</>
 }
