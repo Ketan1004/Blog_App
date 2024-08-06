9a6f377c013c16536166ab1837156f3bceb92499
Ketan1004 <ashishshrikhande250@gmail.com>
2024-08-06 10:46:06 +0530

    RTE added

--- Blog/src/components/RTE.jsx
+++ Blog/src/components/RTE.jsx
@@ -1,11 +1,51 @@
 import React from "react";
 import { Editor } from "@tinymce/tinymce-react";
-import {Controller} from 'react-hook-form';
+import { Controller } from "react-hook-form";
 
-
-
-export default function RTE() {
+export default function RTE({ name, control, label, defaultvalue = "" }) {
   return (
-   <div>Test</div>
+    <div className="w-full">
+      {label && <label className="inlinr-block mb-1 pl-1">{label}</label>}
+      <Controller
+        name={name || "content"}
+        control={control}
+        render={({ field: { onChange } }) => (
+          <Editor
+            initialValue={defaultvalue}
+            init={{
+              height: 500,
+              menubar: true,
+              plugins: [
+                "image",
+                "advlist",
+                "lists",
+                "link",
+                "image",
+                "charmap",
+                "preview",
+                "anchor",
+                "searchreplace",
+                "visualblocks",
+                "code",
+                "fullscreen",
+                "insertdatetime",
+                "table",
+                "media",
+                "code",
+                "wordcount",
+                "anchor",
+              ],
+              toolbar:
+                "undo redo | formatselect | bold italic underline |\
+                 alignleft aligncenter alignright alignjustify |\
+                  bullist numlist outdent indent | removeformat | help",
+              content_style:
+                " body{ font-family:Helvetica,Arial,sans-serif; font-size-14px}",
+            }}
+            onEditorChange={onChange}
+          />
+        )}
+      />
+    </div>
   );
 }
